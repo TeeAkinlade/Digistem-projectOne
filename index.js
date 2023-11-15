36fe327be53655a521158b58ba21c440689e2d33
TeeAkinlade <taiye.akinlade!@gmail.com>
2023-11-15 20:24:15 +0100

    responsiveness

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const menu = document.querySelector('ul')
-const openIcon = document.querySelector('.openIcon')
-const closeIcon = document.querySelector('.closeIcon')
-const enrolBtn = document.querySelector('.btn1')
+const menu = document.querySelector('ul');
+const openIcon = document.querySelector('.openIcon');
+const closeIcon = document.querySelector('.closeIcon');
 
 
 openIcon.addEventListener('click', () =>{
     menu.style.display ='block';
     closeIcon.style.display = "block";
-    enrolBtn.style.display = "block";
 })
 closeIcon.addEventListener('click', () =>{
   menu.style.display ='none';
